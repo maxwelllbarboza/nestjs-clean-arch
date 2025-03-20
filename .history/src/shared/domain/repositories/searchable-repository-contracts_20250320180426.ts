2cce0aa758ea8d466414b3ff535c72d393c019d1
Maxwell <Maxwellbarboza@hotmail.com>
2025-03-20 18:14:06 -0300

    Adding SearchResult Class

--- .history/src/shared/domain/repositories/searchable-repository-contracts_20250320180426.ts
+++ .history/src/shared/domain/repositories/searchable-repository-contracts_20250320180426.ts
@@ -96,41 +96,6 @@ export class SearchParams {
   }
 }
 
-export class SearchResult<E extends Entity, Filter = string> {
-  readonly items: E[];
-  readonly total: number;
-  readonly currentPage: number;
-  readonly perPage: number;
-  readonly lastPage: number;
-  readonly sort: string | null;
-  readonly sortDir: string | null;
-  readonly filter: Filter | null;
-
-  constructor(props: SearchResultProps<E, Filter>) {
-    this.items = props.items;
-    this.total = props.total;
-    this.currentPage = props.currentPage;
-    this.perPage = props.perPage;
-    this.lastPage = Math.ceil(this.total / this.perPage);
-    this.sort = props.sort ?? null;
-    this.sortDir = props.sortDir ?? null;
-    this.filter = props.filter ?? null;
-  }
-
-  toJSON(forceEntity = false) {
-    return {
-      items: forceEntity ? this.items.map((item) => item.toJSON()) : this.items,
-      total: this.total,
-      currentPage: this.currentPage,
-      perPage: this.perPage,
-      lastPage: this.lastPage,
-      sort: this.sort,
-      sortDir: this.sortDir,
-      filter: this.filter,
-    };
-  }
-}
-
 export interface SerchableRepositoryInterface<
   E extends Entity,
   SearchInput,
