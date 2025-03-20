4b5443bc107a35a337e4001676eefe002787f6c6
Maxwell <Maxwellbarboza@hotmail.com>
2025-03-20 18:31:08 -0300

    Adding search method in InMemory Class

---
+++ .history/src/shared/domain/repositories/in-memory.searchable.repository_20250320182510.ts
@@ -0,0 +1,13 @@
+import { Entity } from '../entities/entity';
+import { InMemoryRepository } from './in-memory.repository';
+
+import { SearchParams, SerchableRepositoryInterface } from './searchable-repository-contracts';
+
+export abstract class InMemorySearchableRepository<E extends Entity>
+  extends InMemoryRepository<E>
+  implements SerchableRepositoryInterface<E, any, any>
+{
+  async search(props: SearchParams): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+}
